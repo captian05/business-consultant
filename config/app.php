255aae6dc06c73c4ce9e84aa189ba77d8134c91c
Harish V P <93824602+captian05@users.noreply.github.com>
2026-02-06 11:20:58 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -54,9 +54,7 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
-
-    'url' => env('APP_URL', 'http://localhost'),
-
+    'url' => env('APP_URL', 'https://business-consultant.onrender.com'),
     'asset_url' => env('ASSET_URL'),
 
     /*
